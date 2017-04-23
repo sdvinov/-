3dd2452f96cfb985a3c4807484f1cd9c3951a235
sdvinov <odvakr.wikia@gmail.com>
2017-04-23 19:34:53 -0400

    fixes and comments

--- blue/js/main.js
+++ blue/js/main.js
@@ -2,18 +2,21 @@ $(document).ready(function()
 {
 	$(window).scroll(function ()
 	{
-    var margin = $('header a#head').height();
+		// Measure current margin between top and menu
+    let margin = $('header a#head').height();
+
+		// If there is gap between top and menu
     if ($(window).scrollTop() > margin)
     {
+			// Add classes to stick menu at top
     	$('header nav').addClass('fixed-nav');
 			$('#info').addClass('info-bg');
     }
     else
     {
+			// Remove classes and put it back to its' original place
     	$('header nav').removeClass('fixed-nav');
 			$('#info').removeClass('info-bg');
     }
-    return false;
 	});
-	$('.news-item').wrapInner('<a href="#"></a>');
 });
